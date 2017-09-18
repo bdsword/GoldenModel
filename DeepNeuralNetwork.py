46b0c46bdbd041f5dff1ce027205c30a1a6b89ef
BDSword <thetruebloosword@gmail.com>
2017-09-18 17:17:27 +0800

    Extract DeepNeuralNetwork.py from main.py.

---
+++ DeepNeuralNetwork.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+
+class DeepNeuralNetwork:
+    def __init__(self, input_dim, output_dim, layers_conf, learning_rate=0.5):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.layers_conf = layers_conf
+        self.learning_rate = learning_rate
+
+        self.layers_matrix = []
+        self.biases_matrix = []
+        self.probs_matrix = []
+
+        self._graph = None
+
+    def build_graph(self):
+        self._graph = tf.Graph()
+        with self._graph.as_default():
+            self.inputs = tf.placeholder(dtype=tf.float32, shape=[
+                                    None, self.input_dim])
+            self.labels = tf.placeholder(dtype=tf.int32, shape=[None])
+            one_hot_labels = tf.one_hot(self.labels, depth=self.output_dim) 
+
+            self.probs_matrix.append(self.inputs)
+            for i in range(len(self.layers_conf)):
+                if i == 0:
+                    mat = tf.Variable(tf.random_normal([self.input_dim, self.layers_conf[i]]))
+                else:
+                    mat = tf.Variable(tf.random_normal(
+                        [self.layers_conf[i - 1], self.layers_conf[i]]))
+                bias = tf.Variable(tf.random_normal([self.layers_conf[i]]))
+                self.layers_matrix.append(mat)
+                self.biases_matrix.append(bias)
+                prob = tf.nn.relu(tf.add(tf.matmul(self.probs_matrix[-1], mat), bias))
+                self.probs_matrix.append(prob)
+
+            mat = tf.Variable(tf.random_normal([self.layers_conf[-1], self.output_dim]))
+            bias = tf.Variable(tf.random_normal([self.output_dim]))
+            self.layers_matrix.append(mat)
+            self.biases_matrix.append(bias)
+
+            logits = tf.add(tf.matmul(self.probs_matrix[-1], mat), bias)
+            prob = tf.nn.softmax(logits)
+            self.predicts = tf.argmax(prob, 1)
+            self.loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_labels))
+            optimizer = tf.train.AdamOptimizer()
+            self.train_op = optimizer.minimize(self.loss_op)
+
+            # Test accuracy
+            self.acc, self.acc_op = tf.metrics.accuracy(labels=self.labels, predictions=self.predicts)
+
+            self.init = tf.global_variables_initializer()
+            self.init_local = tf.local_variables_initializer()
+
+    def init_variables(self, session):
+        session.run(self.init)
+        session.run(self.init_local)
+
+    def get_graph(self):
+        if self._graph is None:
+            raise Exception('Please build the graph first.')
+        return self._graph
+
+    def get_accuracy(self, sess, test_examples, test_labels):
+        _, cur_acc = sess.run([self.acc_op, self.acc], feed_dict={self.inputs: test_examples, self.labels: test_labels})
+        return cur_acc
+
+    def train(self, sess, batch_examples, batch_labels):
+        _, loss = sess.run([self.train_op, self.loss_op], feed_dict={
+                self.inputs: batch_examples, self.labels: batch_labels})
+        return loss
+
--- main.py
+++ main.py
@@ -1,107 +1,50 @@
-#!/usr/bin/env python3
-
+from FeatureReader import FeatureReader
+from DeepNeuralNetwork import DeepNeuralNetwork
 import tensorflow as tf
 import numpy as np
 import sys
 import os
-import collections
-from FeatureReader import FeatureReader
-from tensorflow.examples.tutorials.mnist import input_data
 
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="2"
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-
 FLAGS = tf.app.flags.FLAGS
 
-def main(_):
-
+def read_data():
     train_examples, train_labels, test_examples, test_labels = FeatureReader(range(12), [6, 6, 6, 6, 6, 6, 0, 1, 6, 6, 0, 5], 'features').read()
     print('train_examples: ', np.shape(train_examples))
     print('train_labels: ', np.shape(train_labels))
     print('test_examples: ', np.shape(test_examples))
     print('test_labels: ', np.shape(test_labels))
+    return train_examples, train_labels, test_examples, test_labels
 
-    # MNIST 
-    # input_dim = 784
-    # output_dim = 10
+def main(_):
+    train_examples, train_labels, test_examples, test_labels = read_data()
+    dnn = DeepNeuralNetwork(np.shape(train_examples)[1], 2, [1024, 512, 256, 128, 32])
+    dnn.build_graph()
 
-    input_dim = np.shape(train_examples)[1]
-    output_dim = 2
-    batch_size = 10
-    num_preprocess_threads = 1
-    min_queue_examples = 256
+    batch_size = 200
 
-    learning_rate = 0.5
-    layer_conf = [50, 50, 50, 50, 50, 50, 50]
-    layer_matrix = []
-    bias_matrix = []
-    prob_matrix = []
-    graph = tf.Graph()
-    with graph.as_default():
-        inputs = tf.placeholder(dtype=tf.float32, shape=[
-                                None, input_dim])
-        labels = tf.placeholder(dtype=tf.int32, shape=[None])
-        one_hot_labels = tf.one_hot(labels, depth=output_dim) 
+    with tf.Session(graph=dnn.get_graph()) as sess:
+        dnn.init_variables(sess)
 
-        prob_matrix.append(inputs)
-        for i in range(len(layer_conf)):
-            if i == 0:
-                mat = tf.Variable(tf.random_normal([input_dim, layer_conf[i]]))
-            else:
-                mat = tf.Variable(tf.random_normal(
-                    [layer_conf[i - 1], layer_conf[i]]))
-            bias = tf.Variable(tf.random_normal([layer_conf[i]]))
-            layer_matrix.append(mat)
-            bias_matrix.append(bias)
-            prob = tf.nn.relu(tf.add(tf.matmul(prob_matrix[-1], mat), bias))
-            prob_matrix.append(prob)
-
-        mat = tf.Variable(tf.random_normal([layer_conf[-1], output_dim]))
-        bias = tf.Variable(tf.random_normal([output_dim]))
-        layer_matrix.append(mat)
-        bias_matrix.append(bias)
-
-        logits = tf.add(tf.matmul(prob_matrix[-1], mat), bias)
-        prob = tf.nn.softmax(logits)
-        predicts = tf.argmax(prob, 1)
-        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_labels))
-        optimizer = tf.train.AdamOptimizer()
-        train_op = optimizer.minimize(loss_op)
-
-        # Test accuracy
-        acc, acc_op = tf.metrics.accuracy(labels=labels, predictions=predicts)
-
-        init = tf.global_variables_initializer()
-        init_local = tf.local_variables_initializer()
-
-
-    num_steps = 500000
-    with tf.Session(graph=graph) as sess:
-        sess.run(init)
-        sess.run(init_local)
-
-        batch_examples, batch_labels = tf.train.shuffle_batch([train_examples, train_labels], batch_size=batch_size, capacity=10000, min_after_dequeue=5000, enqueue_many=True)
+        batch_examples, batch_labels = tf.train.shuffle_batch([train_examples, train_labels], batch_size=batch_size, capacity=batch_size * 5, min_after_dequeue=batch_size * 4, enqueue_many=True)
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
 
+        num_steps = 500000
         for i in range(num_steps):
-            # cur_examples, cur_labels = mnist.train.next_batch(batch_size)
             cur_examples, cur_labels = sess.run([batch_examples, batch_labels])
-
-            _, loss = sess.run([train_op, loss_op], feed_dict={
-                    inputs: cur_examples, labels: cur_labels})
+            loss = dnn.train(sess, cur_examples, cur_labels)
 
             if i % 100 == 0:
                 print('loss: ', loss)
             if i % 1000 == 0:
-                _, cur_acc = sess.run([acc_op, acc], feed_dict={inputs: test_examples, labels: test_labels})
-                print('# accuracy: ', cur_acc)
-            
-
+                acc = dnn.get_accuracy(sess, test_examples, test_labels)
+                print('# accuracy: ', acc)
 
 if __name__ == '__main__':
     tf.app.run()
+
